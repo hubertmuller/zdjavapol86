21d0fa0b5fe09dd4f4dd7e8d2981a7e3da6ed2eb
Hubert Muller <hubert@sportport.eu>
2021-08-19 21:17:00 +0200

    powiekszanie;

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 // nasz program
+
+function pomniejsz () {
+    var bodyDokumentu = document.querySelectorAll('body')[0];
+    bodyDokumentu.classList.remove('duze');
+}
+
+function powieksz () {
+    var bodyDokumentu = document.querySelectorAll('body')[0];
+    bodyDokumentu.classList.add('duze');
+}
+
+console.log('przed listenerem');
+
+window.addEventListener('load', function () {
+
+    console.log('zaszlo zdarzenie load');
+    powieksz();
+
+});
+
+console.log('po listenerze');
+
