07e18070b258c413fbe09559f198d82b68b8d9e0
Hubert Muller <hubert@sportport.eu>
2021-08-24 20:19:15 +0200

    walidacja formy

--- js/main.js
+++ js/main.js
@@ -10,14 +10,67 @@ function powieksz () {
     bodyDokumentu.classList.add('duze');
 }
 
+function sprawdz(target) {
+    //var forma = document.querySelectorAll("form")[0];
+    var forma = document.forma;
+    var imieWartosc = forma.imie.value;
+    var nazwiskoWartosc = forma.nazwisko.value;
+    //document.querySelector('#imieError');
+    var imieError = document.getElementById('imieError');
+    var nazwiskoError = document.getElementById('nazwiskoError');
+    var komentarzeError = document.getElementById('komentarzeError');
+    var blad = false;
+
+    var tablicaElementow = [
+        {
+            wartoscSpr: forma.imie.value,
+            poleBlad: imieError
+        },
+        {
+            wartoscSpr: forma.nazwisko.value,
+            poleBlad: nazwiskoError
+        },
+        {
+            wartoscSpr: forma.komentarze.value,
+            poleBlad: komentarzeError
+        }
+    ];
+
+    var ustawBlad = function (status, uchwytPola) {
+        if (status === true) uchwytPola.classList.remove('noerror');
+        else uchwytPola.classList.add('noerror');
+    }
+
+    for(let x = 0; x < tablicaElementow.length ; x++) {
+
+        let sparawdzanyElement = tablicaElementow[x];
+        if (sparawdzanyElement.wartoscSpr.length === 0) {
+            ustawBlad(true, sparawdzanyElement.poleBlad);
+            blad = true;
+        } else {
+            ustawBlad(false, sparawdzanyElement.poleBlad);
+        }
+    }
+
+    if (blad === true) {
+        target.preventDefault();
+    }
+
+
+}
+
 console.log('przed listenerem');
 
 window.addEventListener('load', function () {
-
     console.log('zaszlo zdarzenie load');
     powieksz();
-
+    var forma = document.forma;
+    forma.addEventListener('submit', function (target) {
+        sprawdz(target);
+    });
 });
 
 console.log('po listenerze');
+
+
 
